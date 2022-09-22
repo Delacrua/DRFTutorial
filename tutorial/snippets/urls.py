a3858e20b76d31d3619f7dd5e4135936558acbe7
Uladzimir Fiodarau <ufiodarau@gmail.com>
2022-09-22 18:28:46 +0400

    rewrote views using Response and Request obj

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from snippets.views import *
 
 urlpatterns = [
     path('snippets/', snippet_list),
     path('snippets/<int:pk>/', snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
